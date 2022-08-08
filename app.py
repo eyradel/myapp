a8db0c6cb2eb9ffaf06517c6bc74a0bc4b9cfb12
eyradel <60956082+eyradel@users.noreply.github.com>
2022-08-08 09:58:59 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeClassifier
+
+
 import time
 
 hide_streamlit_style = """
@@ -18,10 +21,11 @@ hide_streamlit_style = """
             <html><body><p></p><body/></html>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.header("DATA MODELLING APPLICATION")
+
 st.markdown("https://delaeyram.com")
 def linear():
-    file = st.file_uploader("Load Your Dataset",['csv'])
+    st.header("DATA MODELLING APPLICATION")
+    file = st.file_uploader("Load Your Dataset",['csv','xlsx'])
     if file is not None:
 
 
@@ -133,11 +137,60 @@ def multi():
 
 
 
+def tree():
+     st.header("Tree")
+     file = st.file_uploader("Load Your Dataset",['csv','xlsx'])
+     if file is not None:
 
+
+         try:
+             data = pd.read_excel(file)
+         except:
+             data = pd.read_csv(file)
+         col1,col2 =  st.columns(2)
+         with col1:
+             if st.checkbox("View(Excel files)"):
+                 st.text(data)
+         with col2:
+             if st.checkbox("View Dataframe(csv files)"):
+                 st.dataframe(data)
+         cell1,cell2 = st.columns(2)
+         with cell1:
+
+             x1 = st.multiselect(" X select only two columns",data.columns)
+
+             x = data[x1]
+
+
+
+         with cell2:
+             y1 = st.multiselect("Y",data.columns)
+             y = data[y1]
+
+     final  =  st.checkbox("Predict")
+     if final:
+        model = DecisionTreeClassifier()
+        model.fit(x,y)
+
+
+        st.markdown("input values to be predicted")
+        g1,g2  = st.columns(2)
+        with g1:
+            ft = st.text_input("Input the first parameter values")
+        with g2:
+            sc = st.text_input("Input the second parameter values")
+     fore = st.checkbox("forecast")
+     if fore:
+         predict = model.predict([[ft,sc]])
+         st.markdown(predict)
 
 
 
 
 
 st.sidebar.image("logo.png")
-multi()
+option = st.sidebar.selectbox("select",['Tree','Regression'])
+if option=='Tree':
+    tree()
+elif option=='Regression':
+    multi()
